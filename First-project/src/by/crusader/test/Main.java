e4cbf9a6e8ec97e0298fb0cbb0da631c71129a00
epyrk <epyrk@DESKTOP-A5HCBE4>
2017-03-23 23:57:58 +0300

    add

--- First-project/src/by/crusader/test/Main.java
+++ First-project/src/by/crusader/test/Main.java
@@ -3,7 +3,7 @@ package by.crusader.test;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Hello world!!nvb!!!!");
+		System.out.println("Hello world!!!!!!");
 
 	}
 
